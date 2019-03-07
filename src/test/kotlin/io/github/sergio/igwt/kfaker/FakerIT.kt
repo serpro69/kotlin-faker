4c27626d7e1d512d59a2bcd75e833f6a2c3cd206
Sergio Pro <sergio.igwt@gmail.com>
2019-03-07 15:00:54 +0100

    Refactor FakerIT test to test for duplicate strings

--- src/test/kotlin/io/github/sergio/igwt/kfaker/FakerIT.kt
+++ src/test/kotlin/io/github/sergio/igwt/kfaker/FakerIT.kt
@@ -2,8 +2,10 @@ package io.github.sergio.igwt.kfaker
 
 import io.github.sergio.igwt.kfaker.provider.FakeDataProvider
 import io.kotlintest.assertSoftly
+import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.matchers.string.shouldNotContain
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldFail
 import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.FreeSpec
 import java.util.Locale
@@ -30,24 +32,73 @@ class FakerIT : FreeSpec({
                     }
                 }
 
-                "THEN each function can be called without exceptions and return type is String" - {
-                    assertSoftly {
-                        providerProps.forEach { (props, provider) ->
-                            props.forEach {
-                                val call = it.getter.call(provider.getter.call(faker))
-                                val returnType = call.toString()
+                "THEN each function is invoked without exceptions" - {
+                    for (i in 0..100) {
+                        assertSoftly {
+                            providerProps.forEach { (props, provider) ->
+                                props.forEach {
+                                    val call = it.getter.call(provider.getter.call(faker))
+                                    val returnType = call.toString()
 
-                                "AND result value for provider: ${provider.name} and fun: ${it.getter.name} should not contain expression characters" {
-                                    when (returnType) {
-                                        "() -> kotlin.String" -> {
-                                            val value = (call as (() -> String)).invoke()
-                                            value shouldNotContain Regex("""(#\{.*\}|#+\p{all}+|\p{all}+#+)""")
-                                        }
-                                        "(String) -> kotlin.String" -> {
-                                            val value = (call as ((String) -> String)).invoke("")
-                                            value shouldNotContain Regex("""(#\{.*\}|#+\p{all}+|\p{all}+#+)""")
-                                        }
-                                        else -> {
+                                    "AND result value for ${provider.name + it.getter.name} should be resolved correctly" {
+                                        val regex = Regex("""#\{.*\}|#++""")
+
+                                        "#{expression}" shouldContain regex
+                                        "### Something with number" shouldContain regex
+                                        "### Something with numbers ###" shouldContain regex
+                                        "######" shouldContain regex
+
+                                        when (returnType) {
+                                            "() -> kotlin.String" -> {
+                                                val value = (call as (() -> String)).invoke()
+                                                if (!value.contains("#chuck and #norris")) value shouldNotContain regex
+
+                                                val values = value.split(" ")
+
+                                                // Check that resolved values are not duplicated
+                                                values.forEachIndexed { index, s ->
+                                                    if (index < values.size - 1) {
+                                                        // Accounting for some exceptional cases where values are repeated in resolved expression
+                                                        if (
+                                                            !value.startsWith("Officer Meow Meow")
+                                                            && !value.startsWith("Hello? Hello?")
+                                                            && !value.startsWith("No, no, no, no, no")
+                                                            && !value.startsWith("Hello? Hello?")
+                                                            && !value.startsWith("Yes. Yes.")
+                                                            && value != "Dance Dance Dance"
+                                                            && value != "Tiger! Tiger!"
+                                                            && (provider.name != "coffee" && it.getter.name != "<get-notes>")
+                                                        ) {
+                                                            values.elementAt(index + 1) shouldNotBe s
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            "(kotlin.String) -> kotlin.String" -> {
+                                                val value = (call as ((String) -> String)).invoke("")
+                                                value shouldNotContain regex
+
+                                                val values = value.split(" ")
+
+                                                // Check that resolved values are not duplicated
+                                                values.forEachIndexed { index, s ->
+                                                    if (index < values.size - 1) values.elementAt(index + 1) shouldNotBe s
+                                                }
+                                            }
+                                            "(kotlin.String) -> kotlin.String!" -> {
+                                                val value = (call as ((String) -> String)).invoke("")
+                                                value shouldNotContain regex
+
+                                                val values = value.split(" ")
+
+                                                // Check that resolved values are not duplicated
+                                                values.forEachIndexed { index, s ->
+                                                    if (index < values.size - 1) values.elementAt(index + 1) shouldNotBe s
+                                                }
+                                            }
+                                            else -> shouldFail {
+                                                println("Unexpected return type $returnType for ${provider.name + it.getter.name}")
+                                            }
                                         }
                                     }
                                 }
