d7e0935dac1b46eb9501441bb9d481a8517090ee
Sergio Pro <sergio.igwt@gmail.com>
2019-09-30 15:10:49 +0200

    Set isolationMode per leaf for config tests

--- src/test/kotlin/io/github/serpro69/kfaker/FakerConfigTest.kt
+++ src/test/kotlin/io/github/serpro69/kfaker/FakerConfigTest.kt
@@ -4,41 +4,52 @@ import io.kotlintest.*
 import io.kotlintest.specs.*
 import java.util.*
 
-class FakerConfigTest : FreeSpec({
-    "GIVEN random is set through FakerConfig" - {
-        Faker.Config.random = Random(42)
-        Faker.init()
+class FakerConfigTest : FreeSpec() {
 
-        val city1 = Faker.address.city()
-        val name1 = Faker.name.name()
+    override fun isolationMode() = IsolationMode.InstancePerLeaf
 
-        "WHEN random is seeded with the same value" - {
+    init {
+        "GIVEN random is set through FakerConfig" - {
             Faker.Config.random = Random(42)
             Faker.init()
 
-            "THEN the output of repeated function calls should be the same" {
-                val city2 = Faker.address.city()
-                val name2 = Faker.name.name()
+            val rand1 = Faker.Config.random
 
-                assertSoftly {
-                    city2 shouldBe city1
-                    name2 shouldBe name1
+            val city1 = Faker.address.city()
+            val name1 = Faker.name.name()
+
+            "WHEN random is seeded with the same value" - {
+                Faker.Config.random = Random(42)
+                Faker.init()
+
+                "THEN the output of repeated function calls should be the same" {
+                    val city2 = Faker.address.city()
+                    val name2 = Faker.name.name()
+
+                    assertSoftly {
+                        city2 shouldBe city1
+                        name2 shouldBe name1
+                    }
                 }
             }
-        }
 
-        "WHEN Faker is re-initialized without setting Faker.Config.random" - {
-            Faker.init()
+            "WHEN Faker is re-initialized without setting Faker.Config.random" - {
+                Faker.init()
 
-            "THEN the output of repeated function calls should be different" {
-                val city2 = Faker.address.city()
-                val name2 = Faker.name.name()
+                val rand2 = Faker.Config.random
 
-                assertSoftly {
-                    city2 shouldNotBe city1
-                    name2 shouldNotBe name1
+                "THEN the output of repeated function calls should be different" {
+                    val city2 = Faker.address.city()
+                    val name2 = Faker.name.name()
+
+                    assertSoftly {
+                        city2 shouldNotBe city1
+                        name2 shouldNotBe name1
+
+                        rand2 shouldNotBe rand1
+                    }
                 }
             }
         }
     }
-})
+}
