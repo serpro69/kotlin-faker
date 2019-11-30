564ce432dc83324a5c100c521d72f61294727d86
Sergio Pro <sergio.igwt@gmail.com>
2019-11-30 15:42:08 +0100

    Change deps scope for logging and groovy libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,21 +20,21 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.codehaus.groovy:groovy:2.5.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.10.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
     implementation("com.github.mifmif:generex:1.0.2")
-    implementation("ch.qos.logback:logback-core:1.2.3")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("io.github.classgraph:classgraph:4.8.56")
+    implementation("org.slf4j:slf4j-api:1.7.28")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
     testImplementation("io.kotlintest:kotlintest-extensions-allure:3.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("ch.qos.logback:logback-core:1.2.3")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+    testRuntimeOnly("org.codehaus.groovy:groovy:2.5.6")
     runtimeOnly(kotlin("script-runtime"))
 }
-
 
 tasks {
     jar {
