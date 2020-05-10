46e20dd058e4f97c508040b07cf7e31cf226cb77
Sergio Pro <sergio.igwt@gmail.com>
2020-05-10 19:59:55 +0200

    Fix git-set-author script

--- git-set-author.sh
+++ git-set-author.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Set git user config so it's reflected in history
-git.config user.name "${GIT_USER_NAME}"
-git.config user.email "${GIT_USER_EMAIL}"
+git config user.name "${GIT_USER_NAME}"
+git config user.email "${GIT_USER_EMAIL}"
