d90251f87100666416671ff1e64d97ed849feb0a
Sergio Pro <sergio.igwt@gmail.com>
2020-05-17 18:36:08 +0200

    Move git config to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+###
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [[ "$_dir" != "$(pwd)" ]]; then
-    cd "$_dir"
+if [[ "${_dir}" != "$(pwd)" ]]; then
+    cd "${_dir}"
 fi
 
 _tag="$(git describe --exact-match --tags HEAD 2>&1)"
-if [[ "$_tag" == *"no tag exactly matches"* ]]; then
+
+if [[ "${_tag}" == *"no tag exactly matches"* ]]; then
     ./gradlew clean tag build bintrayUpload -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
+
+    # Set git author config for the next step to have proper history
+    git config user.name "${GIT_USER_NAME}"
+    git config user.email "${GIT_USER_EMAIL}"
+
+    # Push newly created tag to remote
     git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" --tags
 else
     ./gradlew clean printVersion build bintrayUpload -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
--- git-set-author.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Set git user config so it's reflected in history
-git config user.name "${GIT_USER_NAME}"
-git config user.email "${GIT_USER_EMAIL}"
