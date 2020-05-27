ddfc2db5583532c65db183d51dcc8abc5148ca97
Sergio Pro <sergio.igwt@gmail.com>
2020-05-27 19:22:24 +0200

    Implement lookup command

--- cli-bot/src/main/kotlin/io/github/serpro69/kfaker/app/subcommands/Lookup.kt
+++ cli-bot/src/main/kotlin/io/github/serpro69/kfaker/app/subcommands/Lookup.kt
@@ -1,23 +1,73 @@
 package io.github.serpro69.kfaker.app.subcommands
 
-import io.github.serpro69.kfaker.app.KFaker
+import io.github.serpro69.kfaker.Faker
+import io.github.serpro69.kfaker.FakerConfig
+import io.github.serpro69.kfaker.app.cli.Introspector
+import io.github.serpro69.kfaker.app.cli.Renderer
+import io.github.serpro69.kfaker.create
 import picocli.CommandLine
+import kotlin.system.exitProcess
 
 @CommandLine.Command(
-    name = "lookup"
+    name = "lookup",
+    description = ["lookup functions by name"],
+    mixinStandardHelpOptions = true
 )
-object Lookup : SubCommand() {
-    @CommandLine.Spec
-    override lateinit var spec: CommandLine.Model.CommandSpec
+object Lookup : Runnable {
 
-    @CommandLine.ParentCommand
-    override lateinit var parent: KFaker
+    @CommandLine.Mixin
+    private val options = CommandOptions()
 
-    @CommandLine.Parameters(index = "0")
+    @CommandLine.Parameters(
+        index = "0",
+        description = ["function name to find in each provider", "case-insensitive"]
+    )
     lateinit var functionName: String
 
+    private fun printMatchingFunctions() {
+        val fakerConfig = FakerConfig.builder().create {
+            locale = options.locale
+        }
+
+        val faker = Faker(fakerConfig)
+
+        val introspector = Introspector(faker)
+
+        val filteredMap = introspector.providerFunctions.mapValuesTo(mutableMapOf()) { (_, v) ->
+            v.filter { it.name.toLowerCase().contains(functionName.toLowerCase()) }
+        }.filterValues { it.isNotEmpty() }
+
+        val renderedProviders = if (options.verbose) {
+            filteredMap.map { (provider, functions) ->
+                val renderedFunctions = functions.map {
+                    val value = when (it.parameters.size) {
+                        1 -> it.call(provider.getter.call(faker)).toString()
+                        2 -> it.call(provider.getter.call(faker), "").toString()
+                        3 -> it.call(provider.getter.call(faker), "", "").toString()
+                        else -> exitProcess(3)
+                    }
+
+                    Renderer("${it.name}() // => $value", emptyList())
+                }
+
+                Renderer(provider.name, renderedFunctions)
+            }
+        } else {
+            filteredMap.map { (provider, functions) ->
+                val renderedFunctions = functions.map {
+                    Renderer("${it.name}()", emptyList())
+                }
+
+                Renderer(provider.name, renderedFunctions)
+            }
+        }
+
+        val output = Renderer("Faker()", renderedProviders).toString()
+
+        println(output)
+    }
+
     override fun run() {
-        super.run()
-        println("Print providers with functions matching $functionName")
+        printMatchingFunctions()
     }
 }
