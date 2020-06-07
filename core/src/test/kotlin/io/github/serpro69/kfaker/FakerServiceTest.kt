d24434ce8f930b4c06a4f70dd6f53bc253a1d3ec
Sergio Pro <sergio.igwt@gmail.com>
2020-06-07 18:01:07 +0200

    Extract long lists used in tests

--- core/src/test/kotlin/io/github/serpro69/kfaker/FakerServiceTest.kt
+++ core/src/test/kotlin/io/github/serpro69/kfaker/FakerServiceTest.kt
@@ -1,6 +1,7 @@
 package io.github.serpro69.kfaker
 
 import io.github.serpro69.kfaker.dictionary.*
+import io.github.serpro69.kfaker.dictionary.Dictionary
 import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
@@ -25,30 +26,6 @@ internal class FakerServiceTest : DescribeSpec({
             val dictionary = FakerService(faker = Faker(), random = random).dictionary
 
             it("it should contain all keys for 'en' locale") {
-                val dictionaryKeys = listOf(
-                    "address", "ancient", "creature", "app", "appliance", "aqua_teen_hunger_force", "artist",
-                    "back_to_the_future", "bank", "basketball", "beer", "blood", "bojack_horseman", "book", "bossa_nova",
-                    "breaking_bad", "buffy", "business", "cannabis", "chiquito", "chuck_norris", "code", "coffee",
-                    "coin", "color", "commerce", "community", "company", "compass", "computer", "construction",
-                    "cosmere", "crypto_coin", "culture_series", "currency", "dc_comics", "demographic", "dessert",
-                    "device", "dnd", "game", "games", "dragon_ball", "dr_who", "dumb_and_dumber", "dune", "educator",
-                    "electrical_components", "esport", "family_guy", "file", "finance", "food", "football",
-                    "the_fresh_prince_of_bel_air", "friends", "funny_name", "game_of_thrones", "gender", "ghostbusters",
-                    "grateful_dead", "greek_philosophers", "hacker", "harry_potter", "heroes", "heroes_of_the_storm",
-                    "hey_arnold", "hipster", "hitchhikers_guide_to_the_galaxy", "hobbit", "house",
-                    "how_i_met_your_mother", "id_number", "industry_segments", "internet", "invoice", "job", "kpop",
-                    "lebowski", "lord_of_the_rings", "lorem", "lovecraft", "markdown", "marketing", "measurement",
-                    "michael_scott", "military", "movie", "music", "name", "nation", "nato_phonetic_alphabet",
-                    "new_girl", "one_piece", "opera", "parks_and_rec", "phish", "phone_number", "cell_phone",
-                    "country_code", "princess_bride", "programming_language", "quote", "rajnikanth", "relationship",
-                    "restaurant", "rick_and_morty", "rock_band", "rupaul", "science", "seinfeld", "shakespeare",
-                    "silicon_valley", "simpsons", "slack_emoji", "source", "south_park", "space", "stargate",
-                    "star_trek", "star_wars", "stranger_things", "stripe", "subscription", "superhero",
-                    "sword_art_online", "team", "the_expanse", "the_it_crowd", "the_thick_of_it", "twin_peaks",
-                    "umphreys_mcgee", "university", "vehicle", "venture_bros", "verbs", "v_for_vendetta", "world_cup",
-                    "yoda", "separator"
-                )
-
                 dictionary.categories.map { it.categoryName.toLowerCase() } shouldContainExactlyInAnyOrder dictionaryKeys
             }
 
@@ -131,15 +108,7 @@ internal class FakerServiceTest : DescribeSpec({
             val category = fakerService.fetchCategory(CategoryName.ADDRESS)
 
             it("category map should contain all its keys") {
-                val categoryKeys = listOf(
-                    "city_prefix", "city_suffix", "country", "country_by_code", "country_by_name", "country_code",
-                    "country_code_long", "building_number", "community_prefix", "community_suffix", "community",
-                    "street_suffix", "secondary_address", "postcode", "postcode_by_state", "state", "state_abbr",
-                    "time_zone", "city", "city_with_state", "street_name", "street_address", "full_address", "mail_box",
-                    "default_country"
-                )
-
-                category.values.keys shouldContainAll categoryKeys
+                category.values.keys shouldContainAll addressCategoryKeys
             }
         }
 
