a45b1e700363d1bd75b368b052700d728f045dad
Sergio Pro <sergio.igwt@gmail.com>
2020-06-28 18:07:00 +0200

    Build native image before uploading to bintray
    It's better to upload the artifacts to bintray only if the native
    image was built successfully.

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ git config --local user.email "${GIT_USER_EMAIL}"
 _tag="$(git describe --exact-match --tags HEAD 2>&1)"
 
 if [[ "${_tag}" == *"no tag exactly matches"* ]]; then
-    ./gradlew clean tag build core:bintrayUpload cli-bot:nativeImage -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
+    ./gradlew clean tag build cli-bot:nativeImage core:bintrayUpload -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
 else
     if [[ "${_tag}" == *"No names found, cannot describe anything"* ]]; then
         echo "Setting tag for first release"
         git tag v0.0.1
     fi
 
-    ./gradlew clean printVersion build core:bintrayUpload cli-bot:nativeImage -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
+    ./gradlew clean printVersion build cli-bot:nativeImage core:bintrayUpload -Prelease -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY}
 fi
