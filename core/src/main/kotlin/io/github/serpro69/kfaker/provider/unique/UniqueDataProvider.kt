b1ee0ffb16222b3b3010d1736e019e5774fed9a7
Sergio Pro <sergio.igwt@gmail.com>
2020-12-12 14:50:54 +0100

    Remove unused code from provider/unique/* classes

--- core/src/main/kotlin/io/github/serpro69/kfaker/provider/unique/UniqueDataProvider.kt
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/unique/UniqueDataProvider.kt
@@ -3,17 +3,6 @@ package io.github.serpro69.kfaker.provider.unique
 abstract class UniqueDataProvider {
     internal abstract val config: UniqueProviderConfiguration
 
-//    /**
-//     * A set of providers that are configured to return unique values.
-//     */
-//    internal abstract val markedUnique: Set<*>
-//
-//    /**
-//     * A map of key=value pairs representing already returned (used) values
-//     * which will not be returned again.
-//     */
-//    internal abstract val usedValues: Map<*, *>
-
     /**
      * Disables "unique generation" for all providers that were configured to return unique values.
      */
