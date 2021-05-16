cd6830e1eca79b95f324488698a39dbd6c3fa64c
Sergio Pro <sergio.igwt@gmail.com>
2021-05-16 14:44:43 +0200

    Add 'seed' param to FakerConfig.Builder

--- core/src/main/kotlin/io/github/serpro69/kfaker/FakerConfig.kt
+++ core/src/main/kotlin/io/github/serpro69/kfaker/FakerConfig.kt
@@ -10,22 +10,43 @@ class FakerConfig private constructor(
     val uniqueGeneratorRetryLimit: Int
 ) {
 
+    private constructor(
+        locale: String,
+        seed: Long,
+        uniqueGeneratorRetryLimit: Int
+    ) : this(locale, Random(seed), uniqueGeneratorRetryLimit)
+
     companion object {
         @JvmStatic
         fun builder() = Builder()
     }
 
+    /**
+     * @property locale Sets the [FakerConfig.locale] when creating an instance of [FakerConfig] with this [Builder].
+     * Default: `"en"`.
+     *
+     * @property random Sets the [FakerConfig.random] when creating an instance of [FakerConfig] with this [Builder].
+     * Default: `"en"`.
+     *
+     * @property randomSeed Sets the seed for [FakerConfig.random].
+     * If specified, the [random] property will be ignored
+     * when creating an instance of [FakerConfig] with this [Builder].
+     * Default: `null`.
+     *
+     * @property uniqueGeneratorRetryLimit Sets the [FakerConfig.uniqueGeneratorRetryLimit]
+     * when creating an instance of [FakerConfig] with this [Builder].
+     * Default: `100`.
+     */
     @FakerDsl
     class Builder internal constructor() {
         var locale = "en"
         var random = Random()
+        var randomSeed: Long? = null
         var uniqueGeneratorRetryLimit = 100
 
-        internal fun build() = FakerConfig(
-            locale,
-            random,
-            uniqueGeneratorRetryLimit
-        )
+        internal fun build() = randomSeed?.let {
+            FakerConfig(locale, it, uniqueGeneratorRetryLimit)
+        } ?: FakerConfig(locale, random, uniqueGeneratorRetryLimit)
     }
 }
 
