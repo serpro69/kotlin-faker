4e88817a7dd197cd67769f9fbb8d3f56008af688
Sergio Pro <sergio.igwt@gmail.com>
2021-06-07 21:38:34 +0200

    [docs] Rename data-provider Dictionary code snippet names

--- prepare-dicts-for-docs.sh
+++ prepare-dicts-for-docs.sh
@@ -1,5 +1,26 @@
-for i in core/src/main/resources/locales/en/*.yml; do
-  # IF first line does not already have a comment
-  n="provider_${i%[.]*}"
-  sed -i -e '1i# START '"$n"'' -e '$a# END '"$n"'' -- "$i"
+#!/bin/bash
+######
+
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ "${_dir}" != "$(pwd)" ]]; then
+    cd "${_dir}" || exit 1
+fi
+
+cd ./core/src/main/resources/locales/en || exit 1
+
+for i in ./*.yml; do
+  if head -n 1 "${i}" | grep -qe '^# START' && tail -n 1 "${i}" | grep -qe "^# END" ; then
+    echo "nothing to do" >/dev/null 2>&1
+  elif head -n 1 "${i}" | grep -qe '^# START' ; then
+    echo "${i} does not match code snippet pattern on last line"
+  elif tail -n 1 "${i}" | grep -qe "^# END" ; then
+    echo "${i} does not match code snippet pattern on first line"
+  else
+    echo "modifying ${i}"
+    s="${i:2}"
+    n="${s%[.]*}_provider_dict"
+    sed -i -e '1i# START '"$n"'' -e '$a# END '"$n"'' -- "$i"
+  fi
 done
+
+cd "${_dir}" || exit 1
