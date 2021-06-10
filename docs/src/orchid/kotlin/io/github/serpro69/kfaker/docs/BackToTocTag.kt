f30eca26516174b038df5492ca49d5f3227e3201
Sergio Pro <sergio.igwt@gmail.com>
2021-06-10 21:19:29 +0200

    [docs] Add 'box' and 'btc' (back-to-toc) tags

---
+++ docs/src/orchid/kotlin/io/github/serpro69/kfaker/docs/BackToTocTag.kt
@@ -0,0 +1,5 @@
+package io.github.serpro69.kfaker.docs
+
+import com.eden.orchid.api.compilers.TemplateTag
+
+class BackToTocTag : TemplateTag("btc", Type.Content, true)
---
+++ docs/src/orchid/kotlin/io/github/serpro69/kfaker/docs/BoxTag.kt
@@ -0,0 +1,5 @@
+package io.github.serpro69.kfaker.docs
+
+import com.eden.orchid.api.compilers.TemplateTag
+
+class BoxTag : TemplateTag("box", Type.Content, true)
