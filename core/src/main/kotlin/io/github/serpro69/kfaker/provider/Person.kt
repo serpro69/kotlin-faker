36032b1dfaf788154f76c2fd83ac8c7bde46e2c3
Sergio Pro <sergio.igwt@gmail.com>
2021-10-16 19:32:37 +0200

    Add birthDate(age, month) to Person provider
    Closes #86

--- core/src/main/kotlin/io/github/serpro69/kfaker/provider/Person.kt
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/Person.kt
@@ -1,6 +1,7 @@
 package io.github.serpro69.kfaker.provider
 
 import java.time.LocalDate
+import java.time.Month
 import java.util.*
 import kotlin.random.asKotlinRandom
 
@@ -15,10 +16,14 @@ class Person internal constructor(private val random: Random) {
     /**
      * Returns a pseudo-random birthDate as [LocalDate] for the given [age] value.
      *
-     * If the [at] date parameter is not provided, then `LocalDate.now()` is used
+     * If the [at] date parameter is not provided, then [LocalDate.now] is used
      * to calculate the "lower bound" for the resulting birthDate.
-     * Therefore, assuming the [age] value of 30, the resulting date will be in the range of:
-     * `LocalDate.now().minusYears(30)..LocalDate.now().minusYears(29).minusDays(1)`
+     * Assuming the [age] value of `30`, the resulting date will be in the range of:
+     *
+     * ```
+     * val start = LocalDate.now().minusYears(30)
+     * val end = LocalDate.now().minusYears(29).minusDays(1)
+     * ```
      *
      * @param age the person's age for which the birthDate will be calculated
      * @param at a date at which the person was of the specified [age].
@@ -32,6 +37,84 @@ class Person internal constructor(private val random: Random) {
         val endDate = startDate.plusYears(1).minusDays(1)
         val lower = LocalDate.ofYearDay(startDate.year, (startDate.dayOfYear..365).random(random.asKotlinRandom()))
         val upper = LocalDate.ofYearDay(endDate.year, (1..endDate.dayOfYear).random(random.asKotlinRandom()))
-        return listOf(lower, upper).random(random.asKotlinRandom())
+        return (lower to upper).random()
     }
+
+    /**
+     * Returns a pseudo-random birthDate as [LocalDate] for the given [age] and [month] values.
+     *
+     * If the [at] date parameter is not provided, then [LocalDate.now] is used to calculate the birth year.
+     * Assuming the [age] value of `30` and [month] of [Month.DECEMBER],
+     * the resulting date will be in the range of:
+     *
+     * ```
+     * val start = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(1)
+     * val end = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(31)
+     * ```
+     *
+     * @param age the person's age for which the birthDate will be calculated
+     * @param month the month for which the birthDate will be calculated
+     * @param at a date at which the person was of the specified [age].
+     * Equals to the result of [LocalDate.now] by default
+     *
+     * @return a pseudo-randomly calculated birthDate as instance of [LocalDate]
+     */
+    @JvmOverloads
+    fun birthDate(age: Long, month: Month, at: LocalDate = LocalDate.now()): LocalDate {
+        return birthDate(age, listOf(month), at = at)
+    }
+
+    /**
+     * Returns a pseudo-random birthDate as [LocalDate] for the given [age] and [months] values.
+     *
+     * If the [at] date parameter is not provided, then [LocalDate.now] is used to calculate the birth year.
+     * Assuming the [age] value of `30` and [months] having [Month.DECEMBER] value,
+     * the resulting date will be in the range of:
+     *
+     * ```
+     * val start = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(1)
+     * val end = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(31)
+     * ```
+     *
+     * @param age the person's age for which the birthDate will be calculated
+     * @param months months for which the birthDate will be calculated
+     * @param at a date at which the person was of the specified [age].
+     * Equals to the result of [LocalDate.now] by default
+     *
+     * @return a pseudo-randomly calculated birthDate as instance of [LocalDate]
+     */
+    @JvmOverloads
+    fun birthDate(age: Long, vararg months: Month, at: LocalDate = LocalDate.now()): LocalDate {
+        val month = months.random(random.asKotlinRandom())
+        val start = at.minusYears(age).withMonth(month.value)
+        val year = start.year
+        val day = (1..month.length(start.isLeapYear)).random(random.asKotlinRandom())
+        return LocalDate.of(year, month, day)
+    }
+
+    /**
+     * Returns a pseudo-random birthDate as [LocalDate] for the given [age] and [months] values.
+     *
+     * If the [at] date parameter is not provided, then [LocalDate.now] is used to calculate the birth year.
+     * Assuming the [age] value of `30` and [months] having [Month.DECEMBER] value,
+     * the resulting date will be in the range of:
+     *
+     * ```
+     * val start = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(1)
+     * val end = LocalDate.now().minusYears(30).withMonth(12).withDayOfMonth(31)
+     * ```
+     *
+     * @param age the person's age for which the birthDate will be calculated
+     * @param months months for which the birthDate will be calculated
+     * @param at a date at which the person was of the specified [age].
+     * Equals to the result of [LocalDate.now] by default
+     *
+     * @return a pseudo-randomly calculated birthDate as instance of [LocalDate]
+     */
+    @JvmOverloads
+    fun birthDate(age: Long, months: List<Month>, at: LocalDate = LocalDate.now()): LocalDate {
+        return birthDate(age, *months.toTypedArray(), at = at)
+    }
+
+    private fun Pair<LocalDate, LocalDate>.random() = listOf(first, second).random(random.asKotlinRandom())
 }
--- core/src/test/kotlin/io/github/serpro69/kfaker/provider/PersonTest.kt
+++ core/src/test/kotlin/io/github/serpro69/kfaker/provider/PersonTest.kt
@@ -2,10 +2,13 @@ package io.github.serpro69.kfaker.provider
 
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.comparables.shouldBeGreaterThanOrEqualTo
 import io.kotest.matchers.comparables.shouldBeLessThanOrEqualTo
 import io.kotest.matchers.shouldBe
 import java.time.LocalDate
+import java.time.Month
+import java.time.Month.DECEMBER
 import java.util.*
 
 class PersonTest : DescribeSpec({
@@ -33,6 +36,36 @@ class PersonTest : DescribeSpec({
                 it("should generate a random birth-date with explicit startDate run#$it") {
                     assertAge(person.birthDate(age, start), start)
                 }
+            }
+        }
+
+        context("birthDate(age, month) fun") {
+            repeat(12) {
+                it("should generate a random birth-date for the given month run#$it") {
+                    val year = LocalDate.now().minusYears(age).year
+                    val month = DECEMBER
+                    val bd = person.birthDate(age, month)
+                    bd shouldBeGreaterThanOrEqualTo LocalDate.of(year, month, 1)
+                    bd shouldBeLessThanOrEqualTo LocalDate.of(year, month, 31)
+                }
+            }
+
+            repeat(91) {
+                it("should generate a random birth-date for one of the given months run#$it") {
+                    val year = LocalDate.now().minusYears(age).year
+                    val months = listOf(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER)
+                    val bd = person.birthDate(age, months)
+                    bd shouldBeGreaterThanOrEqualTo LocalDate.of(year, months.first(), 1)
+                    bd shouldBeLessThanOrEqualTo LocalDate.of(year, months.last(), 30)
+                }
+            }
+
+            it("should take into account leap years for month of February") {
+                val leap = List(100) {
+                    person.birthDate(25, Month.FEBRUARY, at = LocalDate.now().withYear(2025))
+                }
+
+                leap shouldContain LocalDate.parse("2000-02-29")
             }
         }
     }
