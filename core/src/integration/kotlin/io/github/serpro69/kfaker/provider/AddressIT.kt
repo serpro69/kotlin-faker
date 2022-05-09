b1ba373875ad6e4edf60ae7c5804cfc6ff62d5e7
Sergio Pro <sergio.igwt@gmail.com>
2022-05-09 21:09:38 +0200

    Fix Address#postcode for 'nl' locale

--- core/src/integration/kotlin/io/github/serpro69/kfaker/provider/AddressIT.kt
+++ core/src/integration/kotlin/io/github/serpro69/kfaker/provider/AddressIT.kt
@@ -3,6 +3,7 @@ package io.github.serpro69.kfaker.provider
 import io.github.serpro69.kfaker.faker
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.string.shouldMatch
 
 class AddressIT : DescribeSpec({
     describe("Address Provider") {
@@ -23,6 +24,12 @@ class AddressIT : DescribeSpec({
         context("lv locale") {
             it("city() does NOT throw NoSuchElementException") {
                 shouldNotThrow<NoSuchElementException> { address("lv").city() }
+            }
+        }
+
+        context("nl locale") {
+            it("should generate a valid postcode") {
+                address("nl").postcode() shouldMatch Regex("""[1-9]\d{3} \w{2}""")
             }
         }
     }
