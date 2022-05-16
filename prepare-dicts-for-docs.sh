bd9fc64ef98c9ace0436b9e3285ae16a3d22abd1
Sergio Pro <sergio.igwt@gmail.com>
2022-05-16 22:06:08 +0200

    Update 'prepare dicts' function to match better patterns

--- prepare-dicts-for-docs.sh
+++ prepare-dicts-for-docs.sh
@@ -9,16 +9,16 @@ fi
 cd ./core/src/main/resources/locales/en || exit 1
 
 for i in ./*.yml; do
-  if head -n 1 "${i}" | grep -qe '^# START' && tail -n 1 "${i}" | grep -qe "^# END" ; then
+  s="${i:2}"
+  n="${s%[.]*}_provider_dict"
+  if head -n 1 "${i}" | grep -qe "^# START $n" && tail -n 1 "${i}" | grep -qe "^# END $n" ; then
     echo "nothing to do" >/dev/null 2>&1
-  elif head -n 1 "${i}" | grep -qe '^# START' ; then
+  elif head -n 1 "${i}" | grep -qe "^# START $n" ; then
     echo "${i} does not match code snippet pattern on last line"
-  elif tail -n 1 "${i}" | grep -qe "^# END" ; then
+  elif tail -n 1 "${i}" | grep -qe "^# END $n" ; then
     echo "${i} does not match code snippet pattern on first line"
   else
     echo "modifying ${i}"
-    s="${i:2}"
-    n="${s%[.]*}_provider_dict"
     sed -i -e '1i# START '"$n"'' -e '$a# END '"$n"'' -- "$i"
   fi
 done
