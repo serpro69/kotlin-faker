8e5e114bb1673454416b78fe84913b9d06d74056
Sergio Pro <sergio.igwt@gmail.com>
2022-08-11 21:09:28 +0200

    Fix 'address#zip_code' not found in 'fr' locale
    Fix #138

--- core/src/integration/kotlin/io/github/serpro69/kfaker/provider/AddressIT.kt
+++ core/src/integration/kotlin/io/github/serpro69/kfaker/provider/AddressIT.kt
@@ -4,6 +4,7 @@ import io.github.serpro69.kfaker.faker
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldMatch
 
 class AddressIT : DescribeSpec({
@@ -33,14 +34,23 @@ class AddressIT : DescribeSpec({
                 address("nl").postcode() shouldMatch Regex("""[1-9]\d{3} \w{2}""")
             }
         }
+
         context("en-SG locale") {
             it("should generate a valid streetName") {
                 address("en-SG").streetName() shouldNotBe ""
             }
         }
+
         context("ar locale") {
             it("should generate a valid streetName") {
                 address("ar").streetName() shouldNotBe ""
+            }
+        }
+
+        context("fr locale") {
+            it("#138 Parameter 'zip_code' not found in 'ADDRESS' category for 'fr' locale") {
+                address("fr").fullAddress() shouldNotBe ""
+                address("fr").fullAddress() shouldContain Regex("""\d{5}""")
             }
         }
     }
