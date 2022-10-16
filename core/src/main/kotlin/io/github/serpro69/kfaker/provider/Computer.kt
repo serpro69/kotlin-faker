f33148ba01dcafadf200987b5ef9e35412e93c8b
Sergio Pro <sergio.igwt@gmail.com>
2022-10-16 16:10:53 +0200

    Update existing dictionary files

--- core/src/main/kotlin/io/github/serpro69/kfaker/provider/Computer.kt
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/Computer.kt
@@ -18,9 +18,47 @@ class Computer internal constructor(fakerService: FakerService) : YamlFakeDataPr
         fakerService.load(yamlCategory)
     }
 
+    val os by lazy { ComputerOS(fakerService) }
+
     fun type() = resolve("type")
     fun platform() = resolve("platform")
+
+    @Deprecated(
+        message = "This is deprecated and will be removed in future releases",
+        replaceWith = ReplaceWith("os.linus()"),
+        level = DeprecationLevel.WARNING
+    )
     fun linux() = resolve("os", "linux")
+
+    @Deprecated(
+        message = "This is deprecated and will be removed in future releases",
+        replaceWith = ReplaceWith("os.macOS()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun macOS() = resolve("os", "macos")
+
+    @Deprecated(
+        message = "This is deprecated and will be removed in future releases",
+        replaceWith = ReplaceWith("os.windows()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun windows() = resolve("os", "windows")
+}
+
+@Suppress("unused")
+class ComputerOS internal constructor(fakerService: FakerService) : YamlFakeDataProvider<ComputerOS>(fakerService) {
+    override val yamlCategory = YamlCategory.COMPUTER
+    override val localUniqueDataProvider = LocalUniqueDataProvider<ComputerOS>()
+    override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    fun linux() = resolve("os", "linux")
+    fun openBsd() = resolve("os", "obenbsd")
+    fun templeOS() = resolve("os", "templeos")
+    fun plan9() = resolve("os", "plan 9")
     fun macOS() = resolve("os", "macos")
     fun windows() = resolve("os", "windows")
 }
--- core/src/main/kotlin/io/github/serpro69/kfaker/provider/File.kt
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/File.kt
@@ -4,6 +4,7 @@ import io.github.serpro69.kfaker.*
 import io.github.serpro69.kfaker.dictionary.*
 import io.github.serpro69.kfaker.provider.unique.LocalUniqueDataProvider
 import io.github.serpro69.kfaker.provider.unique.UniqueProviderDelegate
+import kotlin.reflect.full.declaredMemberFunctions
 
 /**
  * [FakeDataProvider] implementation for [YamlCategory.FILE] category.
@@ -18,6 +19,34 @@ class File internal constructor(fakerService: FakerService) : YamlFakeDataProvid
         fakerService.load(yamlCategory)
     }
 
+    val mimeType by lazy { FileMimeType(fakerService) }
+
     fun extension() = resolve("extension")
-    fun mimeType() = resolve("mime_type")
+
+    @Deprecated(
+        message = "This is deprecated and will be removed in future releases",
+        replaceWith = ReplaceWith("mimeType.application()"),
+        level = DeprecationLevel.WARNING
+    )
+    fun mimeType() = mimeType::class.declaredMemberFunctions.random().call(mimeType) as String
+}
+
+@Suppress("unused")
+class FileMimeType internal constructor(fakerService: FakerService) : YamlFakeDataProvider<FileMimeType>(fakerService) {
+    override val yamlCategory = YamlCategory.FILE
+    override val localUniqueDataProvider = LocalUniqueDataProvider<FileMimeType>()
+    override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    fun application() = resolve("mime_type", "application")
+    fun audio() = resolve("mime_type", "audio")
+    fun image() = resolve("mime_type", "image")
+    fun message() = resolve("mime_type", "message")
+    fun model() = resolve("mime_type", "model")
+    fun multipart() = resolve("mime_type", "multipart")
+    fun text() = resolve("mime_type", "text")
+    fun video() = resolve("mime_type", "video")
 }
