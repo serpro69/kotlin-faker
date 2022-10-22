f0bcafe95dce1e62e709549407fa77fd7ca2f0dc
Sergio Pro <sergio.igwt@gmail.com>
2022-10-22 20:09:49 +0200

    Update yaml-to-json to support directories

--- buildSrc/src/main/kotlin/yaml-to-json.gradle.kts
+++ buildSrc/src/main/kotlin/yaml-to-json.gradle.kts
@@ -1,7 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.gradle.kotlin.dsl.create
-import kotlin.system.exitProcess
 
 interface Yaml2JsonPluginExtension {
     val input: Property<File>
@@ -19,14 +18,40 @@ class Yaml2JsonPlugin : Plugin<Project> {
             val output = ext.output.get().absoluteFile
 
             doFirst {
-                if (!input.isDirectory) throw IllegalArgumentException("$input is not a directory or does not exist")
+                if (output.exists() && !output.isDirectory) throw IllegalArgumentException("$output is not a directory")
             }
 
             doLast {
-                val map = yamlMapper.readValue(input.inputStream(), Map::class.java)
-                jsonMapper.writeValue(output, map)
+                output.deleteRecursively()
+                if (input.isDirectory) {
+                    input.getYmlFiles().forEach { src ->
+                        val outFile = src.relativeTo(input.parentFile)
+                        val dest = output
+                            .resolve(File("${(outFile.parentFile.resolve(outFile.nameWithoutExtension))}.json"))
+                            .also {
+                                it.parentFile.mkdirs()
+                                it.createNewFile()
+                            }
+                        writeYamlToJson(src, dest)
+                    }
+                } else if (input.extension == "yml" || input.extension == "yaml") {
+                    val dest = output.resolve("${input.nameWithoutExtension}.json").also {
+                        it.parentFile.mkdirs()
+                        it.createNewFile()
+                    }
+                    writeYamlToJson(input, dest)
+                } else throw IllegalArgumentException("Unable to process file $input")
             }
         }
+    }
+
+    private fun File.getYmlFiles(): Sequence<File> {
+        return walk().filter { f -> f.isFile && (f.extension == "yml" || f.extension == "yaml") }
+    }
+
+    private fun writeYamlToJson(src: File, dest: File) {
+        val map = yamlMapper.readValue(src.inputStream(), Map::class.java)
+        jsonMapper.writeValue(dest, map)
     }
 }
 
