b7359e7fc2ae862387791ffa30ab82be6844a609
Sergio Pro <sergio.igwt@gmail.com>
2023-01-22 14:48:41 +0100

    Add new data providers from upstream

---
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/Airport.kt
@@ -0,0 +1,87 @@
+@file:Suppress("unused")
+
+package io.github.serpro69.kfaker.provider
+
+import io.github.serpro69.kfaker.*
+import io.github.serpro69.kfaker.dictionary.*
+import io.github.serpro69.kfaker.provider.unique.LocalUniqueDataProvider
+import io.github.serpro69.kfaker.provider.unique.UniqueProviderDelegate
+import java.util.NoSuchElementException
+
+/**
+ * [FakeDataProvider] implementation for [YamlCategory.AIRPORT] category.
+ */
+class Airport internal constructor(fakerService: FakerService) : YamlFakeDataProvider<Airport>(fakerService) {
+    override val yamlCategory = YamlCategory.AIRPORT
+    override val localUniqueDataProvider = LocalUniqueDataProvider<Airport>()
+    override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    val unitedStates by lazy { UnitedStates(fakerService) }
+    val europeanUnion by lazy { EuropeanUnion(fakerService) }
+}
+
+/**
+ * [FakeDataProvider] implementation for [YamlCategory.AIRPORT] category.
+ */
+class UnitedStates internal constructor(fakerService: FakerService) : YamlFakeDataProvider<UnitedStates>(fakerService) {
+    override val yamlCategory = YamlCategory.AIRPORT
+    override val localUniqueDataProvider = LocalUniqueDataProvider<UnitedStates>()
+    override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    val iataCode by lazy { IataCode(fakerService, "united_states") }
+
+    fun large() = resolve("united_states", "large")
+    fun medium() = resolve("united_states", "medium")
+    fun small() = resolve("united_states", "small")
+}
+
+/**
+ * [FakeDataProvider] implementation for [YamlCategory.AIRPORT] category.
+ */
+class EuropeanUnion internal constructor(fakerService: FakerService) :
+    YamlFakeDataProvider<EuropeanUnion>(fakerService) {
+    override val yamlCategory = YamlCategory.AIRPORT
+    override val localUniqueDataProvider = LocalUniqueDataProvider<EuropeanUnion>()
+    override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    val iataCode by lazy {
+        object : IataCode(fakerService, "european_union") {
+            override fun small(): String {
+                throw NoSuchElementException("airport.european_union.iata_code does not contain values for 'small' key")
+            }
+        }
+    }
+
+    fun large() = resolve("european_union", "large")
+    fun medium() = resolve("european_union", "medium")
+}
+
+/**
+ * [FakeDataProvider] implementation for [YamlCategory.AIRPORT] category.
+ */
+open class IataCode internal constructor(fakerService: FakerService, private val country: String) :
+    YamlFakeDataProvider<IataCode>(fakerService) {
+    final override val yamlCategory = YamlCategory.AIRPORT
+    final override val localUniqueDataProvider = LocalUniqueDataProvider<IataCode>()
+    final override val unique by UniqueProviderDelegate(localUniqueDataProvider)
+
+    init {
+        fakerService.load(yamlCategory)
+    }
+
+    fun large() = resolve(country, "iata_code", "large")
+    fun medium() = resolve(country, "iata_code", "medium")
+    open fun small() = resolve(country, "iata_code", "small")
+}
