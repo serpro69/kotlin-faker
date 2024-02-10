26f748cdde1022499a50b37cac6c9208e0b1a82a
Sergio Pro <22973227+serpro69@users.noreply.github.com>
2024-02-10 21:09:28 +0100

    Add unique to RandomProvider

---
+++ core/src/main/kotlin/io/github/serpro69/kfaker/IRandom.kt
@@ -0,0 +1,242 @@
+package io.github.serpro69.kfaker
+
+import java.util.*
+
+interface IRandom {
+    val config: FakerConfig
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Int] value from this [random] number generator's sequence.
+     */
+    fun nextInt(): Int
+
+    /**
+     * Returns a pseudorandom, uniformly distributed [Int] value between `0` (inclusive)
+     * and the specified [bound] (exclusive),
+     * drawn from this [random] number generator's sequence.
+     */
+    fun nextInt(bound: Int): Int
+
+    /**
+     * Returns a pseudorandom, uniformly distributed [Int] value within the specified int [range] (inclusive),
+     * drawn from this [random] number generator's sequence.
+     */
+    fun nextInt(intRange: IntRange): Int
+
+    /**
+     * Returns a pseudorandom, uniformly distributed [Int] value between [min] (inclusive) and [max] (inclusive),
+     * drawn from this [random] number generator's sequence.
+     */
+    fun nextInt(min: Int, max: Int): Int
+
+    /**
+     * Returns a pseudo-randomly selected value from the [list] of values.
+     */
+    fun <T> randomValue(list: List<T>): T
+
+    /**
+     * Returns a pseudo-randomly selected value from the [array] of values.
+     */
+    fun <T> randomValue(array: Array<T>): T
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Char] value
+     * that corresponds to a letter in the English alphabet.
+     *
+     * @param upper returns the [Char] in upper-case if set to `true`, and in lower-case otherwise
+     */
+    fun nextLetter(upper: Boolean): Char
+
+    /**
+     * Returns [String] with the specified [length] consisting of a pseudo-randomly generated
+     * English alphabet letters and optional [numericalChars],
+     * or an empty string for a `length < 1`.
+     *
+     * @param length         the length of the resulting string.
+     *                       Default: `24`
+     * @param numericalChars add additional numerical chars from 0 to 9 to the resulting string.
+     *                       Default: `true`
+     */
+    fun randomString(length: Int = 24, numericalChars: Boolean = true): String
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Boolean] value
+     * from this random number generator's sequence.
+     *
+     * The values `true` and `false` are produced with (approximately) equal probability.
+     */
+    fun nextBoolean(): Boolean
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Long] value from this [random] number generator's sequence.
+     */
+    fun nextLong(): Long
+
+    /**
+     * Returns a pseudorandom, uniformly distributed [Long] value between `0` (inclusive),
+     * and the specified [bound] value (exclusive),
+     * drawn from this [random] number generator's sequence.
+     *
+     * @throws IllegalArgumentException if `bound < 0`
+     */
+    fun nextLong(bound: Long): Long
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Float] value between `0.0` and `1.0`
+     * from this [random] number generator's sequence.
+     */
+    fun nextFloat(): Float
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Double] value between `0.0` and `1.0`
+     * from this [random] number generator's sequence.
+     */
+    fun nextDouble(): Double
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Char] value,
+     * from this [random] number generator's sequence.
+     */
+    fun nextChar(): Char
+
+    /**
+     * Returns [String] with the specified [length] (or an empty string for a `length < 1`)
+     * consisting of pseudo-randomly generated characters
+     * in a given [locale] with optional [auxiliaryChars] and [numericalChars]
+     *
+     * @param length the length of the resulting string
+     * @param locale locale to use to generate the charset. Defaults to `locale` config value set for the `faker` instance
+     * @param auxiliaryChars add additional auxiliary chars to the resulting string as defined in [Character_Elements](https://www.unicode.org/reports/tr35/tr35-general.html#Character_Elements)
+     * @param numericalChars add additional numerical chars from 0 to 9 to the resulting string
+     */
+    @Deprecated(
+        message = "This function is deprecated and will be removed in future releases.\n" +
+            "Note that default value for 'length' param has changed from '100' to '24' in the new 'randomString' function.",
+        replaceWith = ReplaceWith("randomString"),
+        level = DeprecationLevel.WARNING
+    )
+    fun nextString(
+        length: Int = 100,
+        locale: Locale = Locale.forLanguageTag(config.locale),
+        auxiliaryChars: Boolean = false,
+        numericalChars: Boolean = false
+    ): String
+
+    /**
+     * Returns [String] with the specified [length] (or an empty string for a `length < 1`)
+     * consisting of pseudo-randomly generated characters
+     * in a given [locale] with optional [auxiliaryChars] and [numericalChars]
+     *
+     * @param length         the length of the resulting string.
+     *                       Default: `24`
+     * @param locale         locale to use to generate the charset.
+     *                       Defaults to `locale` config value set for the `faker` instance.
+     * @param indexChars     add additional index chars to the resulting string, as defined in
+     *                       [Character_Elements](https://www.unicode.org/reports/tr35/tr35-general.html#Character_Elements).
+     *                       Default: `true`
+     * @param auxiliaryChars add additional auxiliary chars to the resulting string as defined in
+     *                       [Character_Elements](https://www.unicode.org/reports/tr35/tr35-general.html#Character_Elements).
+     *                       Default: `false`
+     * @param numericalChars add additional numerical chars from 0 to 9 to the resulting string
+     *                       Default: `false`
+     */
+    fun randomString(
+        length: Int = 24,
+        locale: Locale = Locale.forLanguageTag(config.locale),
+        indexChars: Boolean = true,
+        auxiliaryChars: Boolean = false,
+        punctuationChars: Boolean = false,
+        numericalChars: Boolean = false
+    ): String
+
+    /**
+     * Returns [String] with a randomLength withing the specified [min] and [max] boundaries
+     * (or an empty string for if `randomLength < 1`)
+     * consisting of pseudo-randomly generated characters
+     * in a given [locale] with optional [auxiliaryChars] and [numericalChars]
+     *
+     * @param min            the minimum length of the resulting string.
+     * @param max            the maximum length of the resulting string.
+     * @param locale         locale to use to generate the charset.
+     *                       Defaults to `locale` config value set for the `faker` instance.
+     * @param indexChars     add additional index chars to the resulting string, as defined in
+     *                       [Character_Elements](https://www.unicode.org/reports/tr35/tr35-general.html#Character_Elements).
+     *                       Default: `true`
+     * @param auxiliaryChars add additional auxiliary chars to the resulting string as defined in
+     *                       [Character_Elements](https://www.unicode.org/reports/tr35/tr35-general.html#Character_Elements).
+     *                       Default: `false`
+     * @param numericalChars add additional numerical chars from 0 to 9 to the resulting string
+     *                       Default: `false`
+     */
+    fun randomString(
+        min: Int,
+        max: Int,
+        locale: Locale = Locale.forLanguageTag(config.locale),
+        indexChars: Boolean = true,
+        auxiliaryChars: Boolean = false,
+        punctuationChars: Boolean = false,
+        numericalChars: Boolean = false
+    ): String
+
+    /**
+     * Returns a pseudo-randomly selected enum entry of type [E].
+     */
+    fun <E : Enum<E>> nextEnum(enum: Class<E>): E
+
+    /**
+     * Returns a pseudo-randomly selected enum entry from an [Array] of [E] type [values].
+     */
+    fun <E : Enum<E>> nextEnum(values: Array<E>): E
+
+    /**
+     * Returns a pseudo-randomly selected [enum] class of type [E] based on [predicate] for [E].
+     */
+    fun <E : Enum<E>> nextEnum(enum: Class<E>, predicate: (E) -> Boolean): E
+
+    /**
+     * Returns the next pseudorandom [UUID] as [String] taking the seed of this [random].
+     */
+    fun nextUUID(): String
+
+    /**
+     * Returns a view of the portion of the [list]
+     * with pseudo-randomly generated `fromIndex` and (possibly) `toIndex` values.
+     *
+     * @param size the desired size of the resulting list.
+     * If `size <= 0` then `toIndex` will also be randomly-generated.
+     * @param shuffled if `true` the [list] will be shuffled before extracting the sublist
+     */
+    fun <T> randomSublist(list: List<T>, size: Int = 0, shuffled: Boolean = false): List<T>
+
+    /**
+     * Returns a view of the portion of the [list]
+     * with pseudo-randomly generated `fromIndex` and (possibly) `toIndex` values.
+     *
+     * @param sizeRange the desired size range of the resulting list.
+     * The `size` of the returned list is the result of calling [nextInt] with the [sizeRange].
+     * IF `size <= 0` then `toIndex` will also be randomly-generated.
+     * @param shuffled if `true` the [list] will be shuffled before extracting the sublist
+     */
+    fun <T> randomSublist(list: List<T>, sizeRange: IntRange, shuffled: Boolean = false): List<T>
+
+    /**
+     * Returns a portion of the [set]
+     * with pseudo-randomly generated `fromIndex` and (possibly) `toIndex` values.
+     *
+     * @param size the desired size of the resulting set.
+     * If `size <= 0` then `toIndex` will also be randomly-generated.
+     * @param shuffled if `true` the [set] will be shuffled before extracting the subset
+     */
+    fun <T> randomSubset(set: Set<T>, size: Int = 0, shuffled: Boolean = false): Set<T>
+
+    /**
+     * Returns a portion of the [set]
+     * with pseudo-randomly generated `fromIndex` and (possibly) `toIndex` values.
+     *
+     * @param sizeRange the desired size range of the resulting list.
+     * The `size` of the returned list is the result of calling [nextInt] with the [sizeRange].
+     * IF `size <= 0` then `toIndex` will also be randomly-generated.
+     * @param shuffled if `true` the [set] will be shuffled before extracting the subset
+     */
+    fun <T> randomSubset(set: Set<T>, sizeRange: IntRange, shuffled: Boolean = false): Set<T>
+}
---
+++ core/src/main/kotlin/io/github/serpro69/kfaker/provider/misc/RandomProvider.kt
@@ -0,0 +1,291 @@
+package io.github.serpro69.kfaker.provider.misc
+
+import io.github.serpro69.kfaker.FakerConfig
+import io.github.serpro69.kfaker.FakerService
+import io.github.serpro69.kfaker.IRandom
+import io.github.serpro69.kfaker.dictionary.Category
+import io.github.serpro69.kfaker.exception.RetryLimitException
+import io.github.serpro69.kfaker.provider.AbstractFakeDataProvider
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_CHAR
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_DOUBLE
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_ENUM
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_FLOAT
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_INT
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_LETTER
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_LONG
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.NEXT_UUID
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.RANDOM_STRING
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.RANDOM_SUBLIST
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.RANDOM_SUBSET
+import io.github.serpro69.kfaker.provider.misc.RandomProvider.Key.RANDOM_VALUE
+import io.github.serpro69.kfaker.provider.unique.LocalUniqueDataProvider
+import io.github.serpro69.kfaker.provider.unique.UniqueDataProvider
+import io.github.serpro69.kfaker.provider.unique.UniqueProviderDelegate
+import java.util.*
+
+class RandomProvider internal constructor(
+    fakerService: FakerService
+) : IRandom, AbstractFakeDataProvider<RandomProvider>(fakerService) {
+    override val category: Category = Category.ofName("RANDOM")
+    override val localUniqueDataProvider = LocalUniqueDataProvider<RandomProvider>()
+    override val unique: RandomProvider by UniqueProviderDelegate(localUniqueDataProvider)
+    override val config: FakerConfig = fakerService.faker.config
+    private val rs = fakerService.randomService
+
+    /**
+     * Clears used unique values for the function [key] of this provider.
+     */
+    fun clear(key: Key) = localUniqueDataProvider.clear(key.name)
+
+    override fun nextInt(): Int = resolveUnique(NEXT_INT, { rs.nextInt() })
+
+    override fun nextInt(bound: Int): Int = resolveUnique(NEXT_INT, { rs.nextInt(bound) })
+
+    override fun nextInt(intRange: IntRange): Int = resolveUnique(NEXT_INT, { rs.nextInt(intRange) })
+
+    override fun nextInt(min: Int, max: Int): Int = resolveUnique(NEXT_INT, { rs.nextInt(min = min, max = max) })
+
+    override fun <T> randomValue(list: List<T>): T = resolveUnique(RANDOM_VALUE, { rs.randomValue(list) })
+
+    override fun <T> randomValue(array: Array<T>): T = resolveUnique(RANDOM_VALUE, { rs.randomValue(array) })
+
+    override fun nextLetter(upper: Boolean): Char = resolveUnique(NEXT_LETTER, { rs.nextLetter(upper) })
+
+    override fun randomString(length: Int, numericalChars: Boolean): String {
+        return resolveUnique(RANDOM_STRING, { rs.randomString(length = length, numericalChars = numericalChars) })
+    }
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed [Boolean] value
+     * from this random number generator's sequence.
+     *
+     * The values `true` and `false` are produced with (approximately) equal probability.
+     *
+     * _NB! this method does not try to produce "unique" values via [unique] data provider._
+     */
+    override fun nextBoolean(): Boolean = rs.nextBoolean()
+
+    override fun nextLong(): Long = resolveUnique(NEXT_LONG, { rs.nextLong() })
+
+    override fun nextLong(bound: Long): Long = resolveUnique(NEXT_LONG, { rs.nextLong(bound) })
+
+    override fun nextFloat(): Float = resolveUnique(NEXT_FLOAT, { rs.nextFloat() })
+
+    override fun nextDouble(): Double = resolveUnique(NEXT_DOUBLE, { rs.nextDouble() })
+
+    override fun nextChar(): Char = resolveUnique(NEXT_CHAR, { rs.nextChar() })
+
+    @Deprecated(
+        message = "This function is deprecated and will be removed in future releases.\n" +
+            "Note that default value for 'length' param has changed from '100' to '24' in the new 'randomString' function.",
+        replaceWith = ReplaceWith("randomString"),
+        level = DeprecationLevel.WARNING
+    )
+    override fun nextString(
+        length: Int,
+        locale: Locale,
+        auxiliaryChars: Boolean,
+        numericalChars: Boolean
+    ): String = resolveUnique(RANDOM_STRING, {
+        rs.randomString(
+            length = length,
+            locale = locale,
+            indexChars = auxiliaryChars,
+            auxiliaryChars = numericalChars
+        )
+    })
+
+    override fun randomString(
+        length: Int,
+        locale: Locale,
+        indexChars: Boolean,
+        auxiliaryChars: Boolean,
+        punctuationChars: Boolean,
+        numericalChars: Boolean
+    ): String = resolveUnique(RANDOM_STRING, {
+        rs.randomString(
+            length = length,
+            locale = locale,
+            indexChars = indexChars,
+            auxiliaryChars = auxiliaryChars,
+            punctuationChars = punctuationChars,
+            numericalChars = numericalChars
+        )
+    })
+
+    override fun randomString(
+        min: Int,
+        max: Int,
+        locale: Locale,
+        indexChars: Boolean,
+        auxiliaryChars: Boolean,
+        punctuationChars: Boolean,
+        numericalChars: Boolean
+    ): String = resolveUnique(RANDOM_STRING, {
+        rs.randomString(
+            min = min,
+            max = max,
+            locale = locale,
+            indexChars = indexChars,
+            auxiliaryChars = auxiliaryChars,
+            punctuationChars = punctuationChars,
+            numericalChars = numericalChars
+        )
+    })
+
+    // copy-pasta from RandomService due to need for reified
+    /**
+     * Returns a randomly selected enum entry of type [E].
+     *
+     * _NB! when used with [unique], the [nextInt], which is used to get a random index of the [enumValues] of [E]
+     * will also use unique generation and will need to be reset via [clear] as well when needed._
+     */
+    inline fun <reified E : Enum<E>> nextEnum(): E = resolveUnique(NEXT_ENUM, {
+        val x: Int = nextInt(enumValues<E>().size)
+        enumValues<E>()[x]
+    })
+
+    override fun <E : Enum<E>> nextEnum(enum: Class<E>): E = resolveUnique(NEXT_ENUM, { rs.nextEnum(enum) })
+
+    override fun <E : Enum<E>> nextEnum(values: Array<E>): E = resolveUnique(NEXT_ENUM, { rs.nextEnum(values) })
+
+    override fun <E : Enum<E>> nextEnum(enum: Class<E>, predicate: (E) -> Boolean): E {
+        return resolveUnique(NEXT_ENUM, { rs.nextEnum(enum, predicate) })
+    }
+
+    // copy-pasta from RandomService due to need for reified
+    /**
+     * Returns a randomly selected enum entry of type [E] excluding a particular enum class by its name.
+     *
+     * _NB! when used with [unique], the [nextInt], which is used to get a random index of the [enumValues] of [E]
+     * will also use unique generation and will need to be reset via [clear] as well when needed._
+     */
+    inline fun <reified E : Enum<E>> nextEnum(vararg excludeName: String): E {
+        do {
+            val e: E = nextEnum()
+            if (excludeName.none { it.lowercase() == e.name.lowercase() }) {
+                return e
+            }
+        } while (true)
+    }
+
+    override fun nextUUID(): String = resolveUnique(NEXT_UUID, { rs.nextUUID() })
+
+    override fun <T> randomSublist(list: List<T>, size: Int, shuffled: Boolean): List<T> {
+        return resolveUnique(RANDOM_SUBLIST, { rs.randomSublist(list = list, size = size, shuffled = shuffled) })
+    }
+
+    override fun <T> randomSublist(list: List<T>, sizeRange: IntRange, shuffled: Boolean): List<T> {
+        return resolveUnique(RANDOM_SUBLIST, {
+            rs.randomSublist(list = list, sizeRange = sizeRange, shuffled = shuffled)
+        })
+    }
+
+    override fun <T> randomSubset(set: Set<T>, size: Int, shuffled: Boolean): Set<T> {
+        return resolveUnique(RANDOM_SUBSET, { rs.randomSubset(set = set, size = size, shuffled = shuffled) })
+    }
+
+    override fun <T> randomSubset(set: Set<T>, sizeRange: IntRange, shuffled: Boolean): Set<T> {
+        return resolveUnique(RANDOM_SUBSET, { rs.randomSubset(set = set, sizeRange = sizeRange, shuffled = shuffled) })
+    }
+
+    fun <T : Any> key(f: () -> T): String {
+        return f.toString()
+    }
+
+    @PublishedApi
+    internal tailrec fun <T> resolveUnique(key: Key, f: () -> T, counter: Int = 0): T {
+        val globalUniqueProvider = fakerService.faker.unique
+        val fakerConfig = fakerService.faker.config
+
+        val result = f.invoke()
+
+        return if (localUniqueDataProvider.markedUnique.contains(this)) {
+            println(localUniqueDataProvider.usedValues)
+            // if function is prefixed with `unique` -> try to resolve a unique value
+            when (val set = localUniqueDataProvider.usedValues[key.name]) {
+                null -> {
+                    localUniqueDataProvider.usedValues[key.name] = mutableSetOf(result.toString())
+                    result
+                }
+                else -> {
+                    if (counter >= fakerConfig.uniqueGeneratorRetryLimit) {
+                        throw RetryLimitException("Retry limit of $counter exceeded")
+                    } else if (!set.contains(result.toString())) result.also {
+                        localUniqueDataProvider.usedValues[key.name] = mutableSetOf(result.toString())
+                            .also { it.addAll(set) }
+                    } else resolveUnique(key, f, counter + 1)
+                }
+            }
+        } else { //if (!globalUniqueProvider.config.markedUnique.contains(this::class)) {
+            // if global unique provider is not enabled for this category -> return result
+            result
+        }
+    }
+
+    /**
+     * Keys for [UniqueDataProvider] to simplify resetting unique values via [clear] function.
+     */
+    enum class Key {
+        /**
+         * Key for [nextInt] function
+         */
+        NEXT_INT,
+
+        /**
+         * Key for [randomValue] function
+         */
+        RANDOM_VALUE,
+
+        /**
+         * Key for [nextLetter] function
+         */
+        NEXT_LETTER,
+
+        /**
+         * Key for [randomString] function
+         */
+        RANDOM_STRING,
+
+        /**
+         * Key for [nextLong] function
+         */
+        NEXT_LONG,
+
+        /**
+         * Key for [nextFloat] function
+         */
+        NEXT_FLOAT,
+
+        /**
+         * Key for [nextDouble] function
+         */
+        NEXT_DOUBLE,
+
+        /**
+         * Key for [nextChar] function
+         */
+        NEXT_CHAR,
+
+        /**
+         * Key for [nextEnum] function
+         */
+        NEXT_ENUM,
+
+        /**
+         * Key for [nextUUID] function
+         */
+        NEXT_UUID,
+
+        /**
+         * Key for [randomSublist] function
+         */
+        RANDOM_SUBLIST,
+
+        /**
+         * Key for [randomSubset] function
+         */
+        RANDOM_SUBSET,
+        ;
+    }
+}
