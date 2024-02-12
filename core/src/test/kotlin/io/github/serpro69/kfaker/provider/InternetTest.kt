505484ed6af6ce98f925903c730a4ef02e3e05f5
Sergio Pro <22973227+serpro69@users.noreply.github.com>
2024-02-12 19:04:53 +0100

    Add IPv6 addr generation to Internet

--- core/src/test/kotlin/io/github/serpro69/kfaker/provider/InternetTest.kt
+++ core/src/test/kotlin/io/github/serpro69/kfaker/provider/InternetTest.kt
@@ -7,6 +7,7 @@ import io.github.serpro69.kfaker.helper.isReservedNet
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
+import sun.net.util.IPAddressUtil
 
 class InternetTest: DescribeSpec({
     describe("Internet provider") {
@@ -17,28 +18,41 @@ class InternetTest: DescribeSpec({
                 it("should generate private IPv4 address run#$it") {
                     isPrivateNet(internet.privateIPv4Address()) shouldBe true
                     isReservedNet(internet.privateIPv4Address()) shouldBe true
+                    IPAddressUtil.isIPv4LiteralAddress(internet.privateIPv4Address()) shouldBe true
                 }
                 it("should generate a public IPv4 address run#$it") {
                     isPrivateNet(internet.publicIPv4Address()) shouldBe false
                     isReservedNet(internet.publicIPv4Address()) shouldBe false
+                    IPAddressUtil.isIPv4LiteralAddress(internet.publicIPv4Address()) shouldBe true
                 }
                 it("should generate a random IPv4 address run#$it") {
+                    IPAddressUtil.isIPv4LiteralAddress(internet.iPv4Address()) shouldBe true
                     internet.iPv4Address().split(".").all { s -> s.toInt() in 0..255 } shouldBe true
+                }
+            }
+        }
+
+        context("IPv6 address generation") {
+            repeat(100) {
+                it("should generate a valid IPv6 address run#$it") {
+                    IPAddressUtil.isIPv6LiteralAddress(internet.iPv6Address()) shouldBe true
                 }
             }
         }
 
         @OptIn(ExperimentalStdlibApi::class)
         context("MacAddress generation") {
-            it("should generate a valid mac-address") {
-                internet.macAddress() shouldMatch Regex("^([0-9a-f]{2}:){5}[0-9a-f]{2}$")
-                internet.macAddress().split(":").all { it.hexToInt() in 0..255 }
-            }
-            it("should generate a valid mac-address with prefix") {
-                internet.macAddress("a").split(":").all { it.hexToInt() in 0..255 }
-                internet.macAddress("aa").split(":").all { it.hexToInt() in 0..255 }
-                internet.macAddress("aa:ce").split(":").all { it.hexToInt() in 0..255 }
-                internet.macAddress("aa:ce:").split(":").all { it.hexToInt() in 0..255 }
+            repeat(100) {
+                it("should generate a valid mac-address run#$it") {
+                    internet.macAddress() shouldMatch Regex("^([0-9a-f]{2}:){5}[0-9a-f]{2}$")
+                    internet.macAddress().split(":").all { s -> s.hexToInt() in 0..255 }
+                }
+                it("should generate a valid mac-address with prefix run#$it") {
+                    internet.macAddress("a").split(":").all { s -> s.hexToInt() in 0..255 }
+                    internet.macAddress("aa").split(":").all { s -> s.hexToInt() in 0..255 }
+                    internet.macAddress("aa:ce").split(":").all { s -> s.hexToInt() in 0..255 }
+                    internet.macAddress("aa:ce:").split(":").all { s -> s.hexToInt() in 0..255 }
+                }
             }
         }
     }
