845166cd2ea69073f07b3acf2e2d932411e08ef1
Sergio Pro <22973227+serpro69@users.noreply.github.com>
2024-02-17 21:26:22 +0100

    Ensure providers' Test tasks depend on :core:shadowJar

--- buildSrc/src/main/kotlin/faker-provider-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/faker-provider-conventions.gradle.kts
@@ -5,8 +5,17 @@ plugins {
 dependencies {
     val compileOnly by configurations
     val testImplementation by configurations
+    // In order to use an additional fake data provider,
+    // core faker needs to be on the classpath.
+    // Don't add it as transitive dependency to each faker provider
     compileOnly(project(":core"))
     // we need implementation dependency for tests to be able to access 'core' functionality
     testImplementation(project(":core"))
+}
+
+// since we're adding :core as testImplementation dependency,
+// we also need to make sure Test tasks depend on core having been built
+tasks.withType<Test> {
+    dependsOn(":core:shadowJar")
 }
 
