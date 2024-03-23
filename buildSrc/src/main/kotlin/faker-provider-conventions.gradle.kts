01f7d376282911f90d085ac69be42b4e9cdb7f26
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2024-03-23 19:37:10 +0100

    Change dependency of 'faker modules' on 'core' to compileOnly
    Need to find a way to make compileOnly work, else each faker module will
    contain everything from core, including shadowed jackson, all the .yml
    dictionaries, etc., etc.

--- buildSrc/src/main/kotlin/faker-provider-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/faker-provider-conventions.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 }
 
 dependencies {
-    val implementation by configurations
-    implementation(project(":core"))
+    val compileOnly by configurations
+    val testImplementation by configurations
+    // In order to use an additional fake data provider,
+    // core faker needs to be on the classpath.
+    // Don't add it as transitive dependency to each faker provider
+    compileOnly(project(":core"))
+    // we need implementation dependency for tests to be able to access 'core' functionality
+    testImplementation(project(":core"))
 }
 
-// since we're adding :core as implementation dependency,
-// we also need to make sure ShadowJar tasks depend on core having been built
+// we have a dependency on :core,
+// hence we also need to make sure ShadowJar tasks depend on core having been built
 val shadowJar by tasks.getting(ShadowJar::class) {
     dependsOn(":core:assemble")
 }
