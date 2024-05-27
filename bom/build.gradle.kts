9aa20cbe9da8f190c69475e3e80b75c017eaef10
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2024-05-27 16:01:41 +0200

    Refactor buildSrc to get rid of duplicates

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -2,27 +2,10 @@ import io.github.serpro69.semverkt.gradle.plugin.tasks.TagTask
 
 plugins {
     `java-platform`
-    `maven-publish`
-    signing
+    `faker-pub-conventions`
 }
 
 val bom = project
-
-val isDev = provider { version.toString().startsWith("0.0.0") }
-val isSnapshot = provider {
-    // QUESTION do we need to check if rootProject is also set to snapshot?
-    //  Likely not, since "isRelease" will not just check for the version, but also for the actual tag creation
-    //    rootProject.version.toString().endsWith("SNAPSHOT") &&
-    version.toString().endsWith("SNAPSHOT")
-}
-val isRelease = provider {
-    val tag = project.tasks.getByName("tag", TagTask::class)
-    /* all fakers have their own tags, so checking if tag.didWork is enough for them,
-       ':core' shares the tag with 'root', ':bom' and ':cli-bot' modules,
-       and hence the tag might already exist and didWork will return false for ':core' */
-    val tagCreated = if (project.name != "core") tag.didWork else tag.didWork || tag.tagExists
-    !isDev.get() && !isSnapshot.get() && tagCreated
-}
 
 // Exclude subprojects that will never be published so that when configuring this project
 // we don't force their configuration and do unnecessary work
@@ -51,77 +34,4 @@ dependencies {
             }
         }
     }
-}
-
-/**
- * For additional modules, we use a combination of rootProject and subproject names for artifact name and similar things,
- * i.e. kotlin-faker-bom
- */
-private val fullName: String = "${rootProject.name}-${project.name}"
-
-publishing {
-    publications {
-        create<MavenPublication>("FakerBom") {
-            from(components["javaPlatform"])
-            groupId = project.group.toString()
-            artifactId = fullName
-            version = project.version.toString()
-            pom {
-                packaging = "pom"
-                name.set("kotlin-faker")
-                description.set("Generate realistically looking fake data such as names, addresses, banking details, and many more, that can be used for testing and data anonymization purposes.")
-                url.set("https://github.com/serpro69/kotlin-faker")
-                scm {
-                    connection.set("scm:git:https://github.com/serpro69/kotlin-faker")
-                    developerConnection.set("scm:git:https://github.com/serpro69")
-                    url.set("https://github.com/serpro69/kotlin-faker")
-                }
-                issueManagement {
-                    url.set("https://github.com/serpro69/kotlin-faker/issues")
-                }
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/mit-license.php")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("serpro69")
-                        name.set("Serhii Prodan")
-                    }
-                }
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["FakerBom"])
-}
-
-/*
- * copy from faker-lib-conventions.gradle.kts:219
- */
-tasks.withType<PublishToMavenRepository>().configureEach {
-    dependsOn(project.tasks.getByName("tag"))
-    dependsOn(project.tasks.withType(Sign::class.java))
-    onlyIf("Not dev") { !isDev.get() }
-    onlyIf("Release or snapshot") { isRelease.get() || isSnapshot.get() }
-}
-
-/*
- * copy from faker-lib-conventions.gradle.kts:226
- */
-tasks.withType<PublishToMavenLocal>().configureEach {
-    onlyIf("In development") { isDev.get() || isSnapshot.get() }
-}
-
-/*
- * copy from faker-lib-conventions.gradle.kts:230
- */
-tasks.withType<Sign>().configureEach {
-    dependsOn(project.tasks.getByName("tag"))
-    onlyIf("Not dev and snapshot") { !isDev.get() && !isSnapshot.get() }
-    onlyIf("Release") { isRelease.get() }
 }
---
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -0,0 +1,59 @@
+import gradle.kotlin.dsl.accessors._617ff5292df7551646490c1442241820.dokkaJavadoc
+import gradle.kotlin.dsl.accessors._617ff5292df7551646490c1442241820.sourceSets
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.configurationcache.extensions.capitalized
+import org.gradle.kotlin.dsl.creating
+import org.gradle.kotlin.dsl.getValue
+
+/**
+ * For additional providers, use a combination of rootProject and subproject names for artifact name and similar things.
+ * i.e. kotlin-faker-books, kotlin-faker-movies, kotlin-faker-tv, ...
+ *
+ * The "core" lib retains the same name as before: kotlin-faker
+ */
+val Project.fullName: String
+    get() = if (name == "core") rootProject.name
+    else "${rootProject.name}-${name}"
+
+val Project.publicationName: String
+    get() = "Faker${path.split(":").joinToString("") { it.capitalized() }}"
+
+private fun createSourcesJarTask(p: Project): Jar {
+    val sourcesJar by p.tasks.creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(p.sourceSets.getByName("main").allSource)
+        from("${p.rootProject.rootDir.resolve("LICENSE.adoc")}") {
+            into("META-INF")
+        }
+    }
+    return sourcesJar
+}
+
+private fun createJavadocJarTask(p: Project): Jar {
+    val dokkaJavadocJar by p.tasks.creating(Jar::class) {
+        archiveClassifier.set("javadoc")
+        dependsOn(p.tasks.dokkaJavadoc)
+        from(p.tasks.dokkaJavadoc.get().outputDirectory.orNull)
+    }
+    return dokkaJavadocJar
+}
+
+val Project.sourcesJar: Jar
+    get() {
+        val sourcesJar = tasks.findByName("sourcesJar")
+        if (sourcesJar != null && sourcesJar is Jar) {
+            return sourcesJar
+        }
+        return createSourcesJarTask(this)
+    }
+
+val Project.dokkaJavadocJar: Jar
+    get() {
+        val dokkaJavadocJar = tasks.findByName("dokkaJavadocJar")
+        if (dokkaJavadocJar != null && dokkaJavadocJar is Jar) {
+            return dokkaJavadocJar
+        }
+        return createJavadocJarTask(this)
+    }
+
