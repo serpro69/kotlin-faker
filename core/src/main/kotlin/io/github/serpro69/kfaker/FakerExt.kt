3f61a1307d8a933021ca03960bcbd14c12f35f57
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-05-21 20:13:09 +0200

    Add randomClassInstance top-level function

---
+++ core/src/main/kotlin/io/github/serpro69/kfaker/FakerExt.kt
@@ -0,0 +1,67 @@
+package io.github.serpro69.kfaker
+
+import io.github.serpro69.kfaker.provider.misc.RandomClassProvider
+import io.github.serpro69.kfaker.provider.misc.RandomProviderConfig
+import kotlin.reflect.KClass
+
+@PublishedApi
+internal val rcp = RandomClassProvider(fakerConfig { })
+
+/**
+ * Creates an instance of [T]. If [T] has a parameterless public/internal constructor then it will be used to create an instance of this class,
+ * otherwise a constructor with minimal number of parameters will be used with randomly-generated values.
+ *
+ * @param configurator configure random class instance creation
+ *
+ * @throws NoSuchElementException if [T] has no public or internal constructor.
+ */
+inline fun <reified T : Any> randomClassInstance(
+    configurator: RandomProviderConfig.() -> Unit = {}
+): T = rcp.randomClassInstance<T>(configurator)
+
+
+/**
+ * Creates an instance of [T]. If [T] has a parameterless public/internal constructor then it will be used to create an instance of this class,
+ * otherwise a constructor with minimal number of parameters will be used with randomly-generated values.
+ *
+ * @param fakerConfig configure the underlying [Faker]
+ * @param configurator configure random class instance creation
+ *
+ * @throws NoSuchElementException if [T] has no public or internal constructor.
+ */
+inline fun <reified T : Any> randomClassInstance(
+    fakerConfig: FakerConfig = fakerConfig { },
+    configurator: RandomProviderConfig.() -> Unit = {}
+): T = RandomClassProvider(fakerConfig).randomClassInstance<T>(configurator)
+
+/**
+ * Creates an instance of [T] from the [KClass] input.
+ * If [T] has a parameterless public/internal constructor then it will be used to create an instance of this class,
+ * otherwise a constructor with minimal number of parameters will be used with randomly-generated values.
+ *
+ * @param kClass a [KClass] of [T]
+ * @param configurator configure random class instance creation
+ *
+ * @throws NoSuchElementException if [T] has no public or internal constructor.
+ */
+fun <T : Any> randomClassInstance(
+    kClass: KClass<T>,
+    configurator: RandomProviderConfig.() -> Unit = {}
+): T = rcp.randomClassInstance(kClass, configurator)
+
+/**
+ * Creates an instance of [T] from the [KClass] input.
+ * If [T] has a parameterless public/internal constructor then it will be used to create an instance of this class,
+ * otherwise a constructor with minimal number of parameters will be used with randomly-generated values.
+ *
+ * @param kClass a [KClass] of [T]
+ * @param fakerConfig configure the underlying [Faker]
+ * @param configurator configure random class instance creation
+ *
+ * @throws NoSuchElementException if [T] has no public or internal constructor.
+ */
+fun <T : Any> randomClassInstance(
+    kClass: KClass<T>,
+    fakerConfig: FakerConfig = fakerConfig { },
+    configurator: RandomProviderConfig.() -> Unit = {}
+): T = RandomClassProvider(fakerConfig).randomClassInstance(kClass, configurator)
