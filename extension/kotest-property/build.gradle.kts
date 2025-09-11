bab2c5d564ad5d7e6a9f37dd4023cbb34be8e610
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-09-11 16:06:15 +0200

    Clean up dependencies in conventions plugins

--- extension/kotest-property/build.gradle.kts
+++ extension/kotest-property/build.gradle.kts
@@ -1,23 +1,3 @@
 plugins { `faker-ext-conventions` }
 
-kotlin {
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                compileOnly(projects.kotlinFaker)
-                compileOnly(libs.test.kotest.property)
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(projects.kotlinFaker)
-                implementation(libs.bundles.test.kotest)
-            }
-        }
-    }
-}
-
-tasks.jvmTest {
-    useJUnitPlatform()
-    dependsOn(":kotlin-faker:jvmJar")
-}
+kotlin { sourceSets { jvmMain { dependencies { compileOnly(libs.test.kotest.property) } } } }
